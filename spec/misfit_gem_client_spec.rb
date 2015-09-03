c6fbada339cdb32415d8280e364859c481bb7dcb
David Faber <david@1bios.co>
2015-09-03 14:28:53 -0700

    Ensure that reconnect creates a new access token

--- spec/misfit_gem_client_spec.rb
+++ spec/misfit_gem_client_spec.rb
@@ -15,5 +15,12 @@ describe MisfitGem::Client do
         client = MisfitGem::Client.new(opts)
       }.to raise_error(MisfitGem::InvalidArgumentError, "Missing required options: consumer_secret")
     end
-  
+
+    it "makes a new access token after reconnecting" do
+      client = described_class.new(consumer_key: 'key',
+                                   consumer_secret: 'secret')
+      token1 = client.reconnect('token1', 'secret1')
+      token2 = client.reconnect('token2', 'secret2')
+      expect(token1).not_to eq(token2)
+    end
 end
